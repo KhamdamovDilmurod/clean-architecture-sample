0c3fd318d4c5f9582017df5daf84ab459c2a01ac
Dilmurod <93697695+KhamdamovDilmurod@users.noreply.github.com>
2024-01-11 10:46:40 +0500

    widgets package added

---
+++ lib/features/todo/presentations/widgets/test_widget.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class TestWidget extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return TestWidgetState();
+  }
+  
+}
+class TestWidgetState extends State<TestWidget>{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Container();
+  }
+  
+}
